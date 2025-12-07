45c88887173084ea9632335ecc349a8dc3cd51fa
Ro <>
2025-12-07 18:10:16 -0300

    config done

--- src/config/environment.config.js
+++ src/config/environment.config.js
@@ -8,7 +8,7 @@ const ENVIRONMENT = {
     GMAIL_PASSWORD: process.env.GMAIL_PASSWORD,
     GMAIL_USER: process.env.GMAIL_USER,
     PORT: process.env.PORT,
-    URL_FRONTEND: process.env.URL_FRONTEND || 'https://mail-project-utn-2025-frontend.vercel.app',
+    URL_FRONTEND: process.env.URL_FRONTEND || 'https://mail-project-frontend-utn-2025.vercel.app',
     JWT_SECRET: process.env.JWT_SECRET,
     MONGO_DB_CONNECTION_STRING: process.env.MONGO_DB_CONNECTION_STRING,
     URL_BACKEND: process.env.URL_BACKEND || 'https://mail-project-utn-2025.vercel.app'
